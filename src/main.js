1ebe3106c653f50f735bbb66650ca847cd80b719
leocastroz <castroz.dev@gmail.com>
2023-01-07 02:22:46 -0300

    add: importing qrcode

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import QrReader from 'vue3-qrcode-reader'
+
 
 
 const app = createApp(App)
-
+app.use(QrReader)
 app.use(router)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,55 @@
 
 
 <template>
-  <div>
-    <h1>HOME PAGE</h1>
+  <div class="">
+    <p>{{error}}</p>
+    <p>{{decodedString}}</p>
+    <button @click="torch=!torch">ATIVAR</button>
+    <qrcode-stream :torch="torch" @init="onInit" @decode="onDecode"></qrcode-stream>
   </div>
 </template>
+
+<script>
+import { QrcodeStream } from 'vue3-qrcode-reader'
+
+export default {
+  data(){
+    return{
+      error: '',
+      decodedString: '',
+      torche: false
+    }
+  },
+  components: {
+    QrcodeStream,
+  },
+  methods: {
+    async onInit(promise){
+      try {
+      const { capabilities } = await promise
+
+      // successfully initialized
+    } catch (error) {
+      if (error.name === 'NotAllowedError') {
+        this.error = "user denied camera access permisson"
+      } else if (error.name === 'NotFoundError') {
+        this.error = "no suitable camera device installed"
+      } else if (error.name === 'NotSupportedError') {
+        this.error = "page is not served over HTTPS (or localhost)"
+      } else if (error.name === 'NotReadableError') {
+        this.error = "maybe camera is already in use"
+      } else if (error.name === 'OverconstrainedError') {
+        this.error = "did you requested the front camera although there is none?"
+      } else if (error.name === 'StreamApiNotSupportedError') {
+        this.error = "browser seems to be lacking features"
+      }
+      } finally {
+        // hide loading indicator
+      }
+    },
+    onDecode(decodedString){
+      this.decodedString = decodedString;
+    }
+  }
+};
+</script>
